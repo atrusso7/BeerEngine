a773169cf018ff61d201d0e00d443b660bf7a6ca
Anthony Russo <atrusso7@gmail.com>
2020-01-29 20:21:55 -0600

    Trained Model
    75% accuracy

---
+++ BeerEngine/BeerEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BeerEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
